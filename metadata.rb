6c886cde99e2488010f44ac634b5238b56f3082b
Patrick Connolly <patrick.c.connolly@gmail.com>
2011-09-08 17:40:54 -0400

    Corrected metadata.rb.

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 maintainer       "Myplanet Digital"
 maintainer_email "patrick@myplanetdigital.com"
 license          "Apache 2.0"
-description      "Installs/Configures phpmd"
+description      "Installs/Configures phploc"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
